33136060de9e19b93c95edc49e28fecb9bbe11ce
duChat <cpantoja022@gmail.com>
2023-09-11 16:55:21 -0600

    Map functions added, api fetch fully functional and optimized, further revision required

---
+++ src/api/APIfetch.js
@@ -0,0 +1,30 @@
+const API = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`;
+import { limit } from '../screens/Pokedex'
+
+
+export default async function fetchPokemonData () {
+
+    const fetchData = async (url) => {
+        const d = await fetch(url);
+        const data = await d.json();
+        return data;
+    }
+
+    const urlList = [];
+    const response = await fetch(API);
+    const res = await response.json();
+    const results = res.results
+    for (const p of results){
+        const url = p.url
+        urlList.push(url);
+    };
+
+    const pokemData = [];
+
+    for(let i = 0; i<limit; i++)
+    {
+        const data = await fetchData(urlList[i]);
+        pokemData.push(data);
+    }
+    return pokemData;
+};
